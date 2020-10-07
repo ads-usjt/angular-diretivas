f35acb5f1575f76520a0366601d9145638440afe
lucasliet <lucasouliveira@gmail.com>
2020-10-06 21:39:46 -0300

    shadow on hover

--- src/app/sombra-na-tabela.directive.ts
+++ src/app/sombra-na-tabela.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSombraNaTabela, table]'
@@ -6,10 +6,24 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 export class SombraNaTabelaDirective {
 
   constructor(private elementRef: ElementRef, private renderer : Renderer2) {
+
+  }
+
+  @HostListener('mouseover')
+  isCursorOver(){
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'box-shadow',
       '10px 10px'
+    )
+  }
+
+  @HostListener('mouseleave')
+  isCursorLeft(){
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'box-shadow',
+      'none'
     )
   }
 
