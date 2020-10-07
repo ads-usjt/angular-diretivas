a3139202bcb418d35c5dce6e87c2d24f3c10b2f7
lucasliet <lucasouliveira@gmail.com>
2020-10-06 21:48:14 -0300

    HostBinding

--- src/app/sombra-na-tabela.directive.ts
+++ src/app/sombra-na-tabela.directive.ts
@@ -1,30 +1,31 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appSombraNaTabela, table]'
+  selector: '[appSombraNaTabela]'
 })
 export class SombraNaTabelaDirective {
+  @HostBinding('style.boxShadow')
+  sombra: string;
 
-  constructor(private elementRef: ElementRef, private renderer : Renderer2) {
-
-  }
+  // constructor(private elementRef: ElementRef, private renderer : Renderer2) {}
 
   @HostListener('mouseover')
   isCursorOver(){
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'box-shadow',
-      '10px 10px'
-    )
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'box-shadow',
+    //   '10px 10px'
+    // )
+    this.sombra = '10px 10px';
   }
 
   @HostListener('mouseleave')
   isCursorLeft(){
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'box-shadow',
-      'none'
-    )
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'box-shadow',
+    //   'none'
+    // )
+    this.sombra = 'none';
   }
-
 }
